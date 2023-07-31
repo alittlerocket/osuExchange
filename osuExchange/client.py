df4afed0db19ac48a2a96d7be4f11297a2bc38ae
trustytrojan <tarbot24@hotmail.com>
2023-07-31 19:00:46 -0400

    update client class

--- osuExchange/client.py
+++ osuExchange/client.py
@@ -5,24 +5,20 @@ from osuExchange.seasonalbg import get_seasonal_backgrounds, SeasonalBackgrounds
 from osuExchange.auth import get_access_token, get_client_credentials_token, ClientCredentialsToken, UserAccessToken, OAuth2Scope
 from osuExchange.typing import GameMode, Literal
 
-class OAuth2Client:
-	def __init__(self):
+class OsuApiClient:
+	def __init__(self, client_id: str, client_secret: str):
 		self.token_obj: ClientCredentialsToken | UserAccessToken | None = None
+		self.client_id = client_id
+		self.client_secret = client_secret
 
 	def get_user_access_token(self,
-		client_id: str, 
-		client_secret: str, 
 		redirect_uri: str,
 		scopes: list[OAuth2Scope]
 	) -> None:
-		self.token_obj = get_access_token(client_id, client_secret, redirect_uri, scopes)
+		self.token_obj = get_access_token(self.client_id, self.client_secret, redirect_uri, scopes)
 		
-	def get_client_credentials_token(
-		self,
-		client_id: str,
-		client_secret: str
-	) -> None:
-		self.token_obj = get_client_credentials_token(client_id, client_secret)
+	def get_client_credentials_token(self) -> None:
+		self.token_obj = get_client_credentials_token(self.client_id, self.client_secret)
 
 	def __raise_authenticate__(self):
 		return Exception('self.token_obj is None; please authenticate')
